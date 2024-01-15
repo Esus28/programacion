298089682c8f1a1864362a1e805e39f4d2424e16
Esus28 <153775640+Esus28@users.noreply.github.com>
2024-01-14 20:14:35 -0600

    ya esta listo

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+    try{
+
+        $conex = new PDO('mysql:host=localhost; dbname=septimo', 'root', '');
+        $conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conex->exec("SET CHARACTER SET utf8");
+
+    }catch(PDOException $e){
+        die('Error: ' . $e->getMessage());
+    }
+
+?>
