cbdbe462860ba6f42096a43d5ec1b8270b03c096
Esus28 <esusjjct10@gmail.com>
2024-01-16 16:06:27 -0600

    nuevo

--- alta_usuario.php
+++ alta_usuario.php
@@ -6,21 +6,11 @@ $nombre = $_POST['nombre'];
 echo($nombre);
 
 $apellidos = $_POST['apellidos'];
-echo($apellidos);
-
 $genero = $_POST['genero'];
-echo($genero);
-
 $domicilio = $_POST['domicilio'];
-echo($domicilio);
-
 $usuario = $_POST['usuario'];
-echo($usuario);
-
 $contrasena = $_POST['password'];
 $cifrado_contra = password_hash($contrasena, PASSWORD_DEFAULT, array("cost"=>12));
-echo($cifrado_contra);
-
 $sql = "INSERT INTO usuarios (id_usuario, nombre, apellidos, genero, domicilio, usuario, contrasena) VALUES (NULL, :nombre, :apellidos, :genero, :domicilio, :usuario, :contrasena)";
 $result = $conex->prepare($sql);
 $result->execute(array(":nombre"=>$nombre, ":apellidos"=>$apellidos, ":genero"=>$genero, ":domicilio"=>$domicilio, ":usuario"=>$usuario, ":contrasena"=>$cifrado_contra));
--- conexion.php
+++ conexion.php
@@ -1,13 +1,15 @@
 <?php
+    //phpinfo();
+    //SQLite3::open("data.sqlite3", SQLITE3_OPEN_CREATE);
+    //$database = new SQLite3('data.sqlite');
 
-    try{
-
-        $conex = new PDO('mysql:host=localhost; dbname=septimo', 'root', '');
+    try {
+        $conex = new PDO('sqlite:data.sqlite3');
         $conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conex->exec("SET CHARACTER SET utf8");
-
-    }catch(PDOException $e){
-        die('Error: ' . $e->getMessage());
+        //$conex->exec("SET CHARACTER SET utf8");
+        
+    } catch(PDOException $e) {
+        die('Error: '. $e->getMessage());
     }
 
 ?>
